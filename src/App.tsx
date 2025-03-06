e36c2c4397b92ccf0454bdcfa2a5b11f9a94a998
Mordecai Machazire <m1506466@sgul.ac.uk>
2025-03-05 21:49:24 -0500

    first-push-from-local

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockChartWithMACD from './stock-chart-dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <StockChartWithMACD />
+    </div>
+  );
+}
+
+export default App;
